b3d24c91e99bb29157e33e4f51aac2e4ad2bcd49
jordan <jordan.lesich@gmail.com>
2022-12-17 17:28:39 -0800

    create useMember hook

--- src/hooks/useMember.tsx
+++ src/hooks/useMember.tsx
@@ -1,5 +1,49 @@
-import React from 'react';
+import { findMember } from '@daohaus/moloch-v3-data';
+import { ValidNetwork, Keychain } from '@daohaus/keychain-utils';
+import { useQuery } from 'react-query';
 
-export const useMember = () => {
-  return null;
+const defaultGraphKeys = { '0x1': import.meta.env.VITE_GRAPH_API_KEY_MAINNET };
+
+const fetchMember = async ({
+  chainId,
+  daoId,
+  memberAddress,
+  graphApiKeys,
+}: {
+  chainId: ValidNetwork;
+  daoId: string;
+  graphApiKeys: Keychain;
+  memberAddress: string;
+}) => {
+  try {
+    const data = await findMember({
+      networkId: chainId,
+      graphApiKeys,
+      dao: daoId,
+      memberAddress,
+    });
+    return data;
+  } catch (error: any) {
+    console.error(error);
+    throw new Error(error?.message as string);
+  }
+};
+
+export const useMember = ({
+  chainId,
+  daoId,
+  memberAddress,
+  graphApiKeys = defaultGraphKeys,
+}: {
+  chainId: ValidNetwork;
+  daoId: string;
+  graphApiKeys?: Keychain;
+  memberAddress: string;
+}) => {
+  const { data, ...rest } = useQuery(
+    [`MolochV3Member/${memberAddress}`, { chainId, daoId, memberAddress }],
+    () => fetchMember({ chainId, daoId, memberAddress, graphApiKeys }),
+    { enabled: !!chainId && !!daoId && !!memberAddress }
+  );
+  return { member: data?.data?.member, ...rest };
 };
--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,9 +1,18 @@
 import { H1 } from '@daohaus/ui';
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import { useMember } from '../hooks/useMember';
+import { TARGET_DAO } from '../targetDAO';
 
 export const Profile = () => {
   const { memberAddress } = useParams();
 
+  const { member } = useMember({
+    daoId: TARGET_DAO.ADDRESS,
+    chainId: TARGET_DAO.CHAIN_ID,
+    memberAddress: memberAddress as string,
+  });
+
+  console.log('member', member);
   return <H1>{memberAddress}</H1>;
 };

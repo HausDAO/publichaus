b74cd99b5fc9f42a863a34858bacb74f25f9d5e6
jordan <jordan.lesich@gmail.com>
2022-12-29 12:11:37 -0800

    create useToken hook

---
+++ src/hooks/useToken.tsx
@@ -0,0 +1,73 @@
+import { LOCAL_ABI } from '@daohaus/abis';
+import { useQuery } from 'react-query';
+
+import { createContract } from '@daohaus/tx-builder';
+import { ValidNetwork, Keychain } from '@daohaus/keychain-utils';
+
+const fetchTokenData = async ({
+  tokenAddress,
+  userAddress,
+  chainId,
+  rpcs,
+}: {
+  tokenAddress: string;
+  userAddress?: string;
+  chainId: ValidNetwork;
+  rpcs?: Keychain;
+}) => {
+  const tokenContract = createContract({
+    address: tokenAddress,
+    abi: LOCAL_ABI.ERC20,
+    chainId,
+    rpcs,
+  });
+
+  try {
+    const decimals = await tokenContract.decimals();
+    const name = await tokenContract.name();
+    const symbol = await tokenContract.symbol();
+    const totalSupply = await tokenContract.totalSupply();
+
+    if (userAddress) {
+      const balance = await tokenContract.balanceOf(userAddress);
+      return {
+        decimals: decimals.toString() as string,
+        name,
+        symbol,
+        balance: balance.toString() as string,
+        totalSupply: totalSupply.toString() as string,
+      };
+    }
+
+    return {
+      decimals: decimals.toString() as string,
+      name,
+      symbol,
+      totalSupply: totalSupply.toString() as string,
+    };
+  } catch (error: any) {
+    console.error(error);
+    throw new Error(error?.message as string);
+  }
+};
+
+export const useToken = ({
+  tokenAddress,
+  userAddress,
+  chainId,
+  rpcs,
+}: {
+  tokenAddress: string;
+  userAddress?: string;
+  chainId: ValidNetwork;
+  rpcs?: Keychain;
+}) => {
+  const { data, error, ...rest } = useQuery(
+    [`tokenData-${tokenAddress}`, { tokenAddress, userAddress, chainId }],
+    () => fetchTokenData({ tokenAddress, userAddress, chainId, rpcs }),
+    {
+      enabled: !!tokenAddress && !!chainId,
+    }
+  );
+  return { tokenData: data, error: error as Error, ...rest };
+};
